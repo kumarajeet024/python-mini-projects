53fef70c469f1945375711bb0713479f50e94d9e
kumarajeet024 <kumarajeet024@gmail.com>
2019-01-13 15:17:16 +0530

    turtle library

---
+++ square.py
@@ -0,0 +1,17 @@
+import turtle
+def draw_square():
+    window = turtle.Screen()
+    window.bgcolor("red")
+
+    ashish = turtle.Turtle()
+    ashish.forward(100)
+    ashish.right(90)
+    ashish.forward(100)
+    ashish.right(90)
+    ashish.forward(100)
+    ashish.right(90)
+    ashish.forward(100)
+    ashish.right(90)
+    window.exitonclick()
+
+draw_square()
